9d478baf7ffa2f3c069fb533ee3cd9ebdce8c610
Karel Nguyen <nguyenkar@seznam.cz>
2019-03-30 20:47:41 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+class Launch {
+  constructor (
+    public id: number,
+    public name: string,
+    public windowStart: string,
+    public windowEnd: string,
+    public locationName: string,
+    public locationCountryCode: string
+  ) {
+    this.id = id
+    this.name = name
+    this.windowStart = windowStart
+    this.windowEnd = windowEnd
+    this.locationName = locationName
+    this.locationCountryCode = locationCountryCode
   }
 }
 
-export default App;
+class App extends React.Component {
+  state = {
+    launches: []
+  }
+
+  componentWillMount () {
+    this.fetchNextLaunches()
+  }
+
+  fetchNextLaunches () {
+    fetch('https://launchlibrary.net/1.4/launch/next/10')
+      .then((res: any) => {
+        res.json()
+          .then((data: any) => {
+            let helperArray: Array<any> = []
+
+            data.launches.map((launch: any) => {
+              helperArray.push(new Launch(
+                launch.id,
+                launch.name,
+                launch.windowstart,
+                launch.windowend,
+                launch.location.name,
+                launch.location.countryCode
+              ))
+            })
+
+            this.setState({ launches: helperArray })
+          })
+      })
+  }
+
+  render() { // exact behaviour as computed properties in Vue
+    const launches = this.state.launches.map((launch: any, key: any) =>
+      <div key={ launch.id }>{ launch.name }</div>
+    )
+    return (
+      <>
+        <header className="App-header">
+          <h1>Space launch</h1>
+          { launches }
+        </header>
+      </>
+    )
+  }
+}
+
+export default App
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="react-scripts" />
+// 
+// // import original module declarations
+// import 'styled-components'
+//
+// // and extend them!
+// declare module 'styled-components' {
+//   export interface DefaultTheme {
+//     borderRadius: string
+//
+//     colors: {
+//       main: string
+//       secondary: string
+//     }
+//   }
+// }
