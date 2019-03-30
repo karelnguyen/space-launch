5220e3b74346bc64e3005cd84c84e267e40bd919
Karel Nguyen <nguyenkar@seznam.cz>
2019-03-30 23:20:24 +0100

    add types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
 class Launch {
-  constructor (
+  constructor(
     public id: number,
     public name: string,
     public windowStart: string,
@@ -9,60 +9,60 @@ class Launch {
     public locationName: string,
     public locationCountryCode: string
   ) {
-    this.id = id
-    this.name = name
-    this.windowStart = windowStart
-    this.windowEnd = windowEnd
-    this.locationName = locationName
-    this.locationCountryCode = locationCountryCode
+    this.id = id;
+    this.name = name;
+    this.windowStart = windowStart;
+    this.windowEnd = windowEnd;
+    this.locationName = locationName;
+    this.locationCountryCode = locationCountryCode;
   }
 }
 
 class App extends React.Component {
   state = {
     launches: []
+  };
+
+  componentWillMount() {
+    this.fetchNextLaunches();
   }
 
-  componentWillMount () {
-    this.fetchNextLaunches()
+  fetchNextLaunches() {
+    fetch("https://launchlibrary.net/1.4/launch/next/10").then((res: any) => {
+      res.json().then((data: any) => {
+        let helperArray: Array<Launch> = [];
+
+        data.launches.map((launch: any) => {
+          helperArray.push(
+            new Launch(
+              launch.id,
+              launch.name,
+              launch.windowstart,
+              launch.windowend,
+              launch.location.name,
+              launch.location.countryCode
+            )
+          );
+        });
+
+        this.setState({ launches: helperArray });
+      });
+    });
   }
 
-  fetchNextLaunches () {
-    fetch('https://launchlibrary.net/1.4/launch/next/10')
-      .then((res: any) => {
-        res.json()
-          .then((data: any) => {
-            let helperArray: Array<any> = []
-
-            data.launches.map((launch: any) => {
-              helperArray.push(new Launch(
-                launch.id,
-                launch.name,
-                launch.windowstart,
-                launch.windowend,
-                launch.location.name,
-                launch.location.countryCode
-              ))
-            })
-
-            this.setState({ launches: helperArray })
-          })
-      })
-  }
-
-  render() { // exact behaviour as computed properties in Vue
-    const launches = this.state.launches.map((launch: any, key: any) =>
-      <div key={ launch.id }>{ launch.name }</div>
-    )
+  render() {
+    const launches = this.state.launches.map((launch: Launch, key: any) => (
+      <div key={launch.id}>{launch.name}</div>
+    ));
     return (
       <>
         <header className="App-header">
           <h1>Space launch</h1>
-          { launches }
+          {launches}
         </header>
       </>
-    )
+    );
   }
 }
 
-export default App
+export default App;
